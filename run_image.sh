f8a75e6b284dc9d340cdf147af57889aca0f3b69
MinhQuanVu <quanvu0604@gmail.com>
2024-03-02 15:02:40 +1100

    docker utsma_demo_img

--- run_image.sh
+++ run_image.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Make sure you put this file and the img in the same directory
+docker image load -i utsma_demo_img
+
+# Give xhost permission to display GUI
+xhost +local:*
+
+##
 docker run --privileged \
            --detach \
-           --restart 'always' \
-           --volume ${PWD}:/root/utsma_ws/src/utsma-sim \
-           --name utsma_dev_container \
-           utsma_dev tail -f /dev/null
+           --name utsma_demo_sim \
+           --network "host" \
+           -e ROS_MASTER_URI="http://localhost:11311" \
+           -e DISPLAY=$DISPLAY \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           utsma_demo_img:latest \
+           bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && \
+           roslaunch utsma_gazebo simulation.launch"
+
+docker run --privileged \
+           --detach \
+           --name utsma_demo_path_planning \
+           --network "host" \
+           -e ROS_MASTER_URI="http://localhost:11311" \
+           -e DISPLAY=$DISPLAY \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           utsma_demo_img:latest \
+           bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && \
+           sleep 10 && \
+           rosrun d_triang utsma_path_planner"
