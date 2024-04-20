35bd56018e776a78ab7418e1d695da49ced09657
Quan Vu <quanvu0604@gmail.com>
2024-04-20 20:37:13 +1000

    for powershell

---
+++ build_image.ps1
@@ -0,0 +1,3 @@
+docker build -f dockerfile/Dockerfile.amd64 `
+             -t utsma_demo_img:latest `
+             .
---
+++ run_image.ps1
@@ -0,0 +1,29 @@
+# PowerShell script version
+
+# Make sure you put this file and the img in the same directory
+docker image load -i utsma_demo_img
+
+# Give xhost permission to display GUI
+& 'C:\Program Files\VcXsrv\xhost.exe' +local:*
+
+# Run the Docker container for simulation
+docker run --privileged `
+           --detach `
+           --name utsma_demo_sim `
+           --network "host" `
+           -e "ROS_MASTER_URI=http://localhost:11311" `
+           -e "DISPLAY=$env:DISPLAY" `
+           -v "/tmp/.X11-unix:/tmp/.X11-unix" `
+           utsma_demo_img:latest `
+           bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && roslaunch utsma_gazebo simulation.launch"
+
+# Run the Docker container for path planning
+docker run --privileged `
+           --detach `
+           --name utsma_demo_path_planning `
+           --network "host" `
+           -e "ROS_MASTER_URI=http://localhost:11311" `
+           -e "DISPLAY=$env:DISPLAY" `
+           -v "/tmp/.X11-unix:/tmp/.X11-unix" `
+           utsma_demo_img:latest `
+           bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && sleep 10 && rosrun d_triang utsma_path_planner"
