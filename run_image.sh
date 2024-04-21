b31f775933e08adfde44b1238670f9278198ca15
MinhQuanVu <quanvu0604@gmail.com>
2024-04-21 17:54:51 +1000

    add code viewer to run_image.sh

--- run_image.sh
+++ run_image.sh
@@ -29,3 +29,14 @@ docker run --privileged \
            bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && \
            sleep 10 && \
            rosrun d_triang utsma_path_planner"
+           
+docker run --privileged \
+           --detach \
+           --name utsma_demo_code_viewer \
+           --network "host" \
+           -e ROS_MASTER_URI="http://localhost:11311" \
+           -e DISPLAY=$DISPLAY \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           utsma_demo_img:latest \
+           bash -c "sleep infinity"
+           
