4c6cac58dbd3772c4ed738dc9fb501822a634dfb
Quan Vu <quanvu0604@gmail.com>
2024-04-28 03:30:58 +1000

    got container working on windows

--- run_image.ps1
+++ run_image.ps1
@@ -1,19 +1,18 @@
 # PowerShell script version
 
 # Make sure you put this file and the img in the same directory
-docker image load -i utsma_demo_img
+#docker image load -i utsma_demo_img
 
 # Give xhost permission to display GUI
-& 'C:\Program Files\VcXsrv\xhost.exe' +local:*
+#& 'C:\Program Files\VcXsrv\xhost.exe' +local:*
 
 # Run the Docker container for simulation
 docker run --privileged `
            --detach `
            --name utsma_demo_sim `
            --network "host" `
-           -e "ROS_MASTER_URI=http://localhost:11311" `
-           -e "DISPLAY=$env:DISPLAY" `
-           -v "/tmp/.X11-unix:/tmp/.X11-unix" `
+           -e ROS_MASTER_URI=http://localhost:11311 `
+           -e DISPLAY=host.docker.internal:0.0 `
            utsma_demo_img:latest `
            bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && roslaunch utsma_gazebo simulation.launch"
 
@@ -22,8 +21,16 @@ docker run --privileged `
            --detach `
            --name utsma_demo_path_planning `
            --network "host" `
-           -e "ROS_MASTER_URI=http://localhost:11311" `
-           -e "DISPLAY=$env:DISPLAY" `
-           -v "/tmp/.X11-unix:/tmp/.X11-unix" `
+           -e ROS_MASTER_URI=http://localhost:11311 `
+           -e DISPLAY=host.docker.internal:0.0 `
            utsma_demo_img:latest `
            bash -c "source /opt/ros/noetic/setup.bash && source /utsma_ws/devel/setup.bash && sleep 10 && rosrun d_triang utsma_path_planner"
+
+docker run --privileged `
+           --detach `
+           --name utsma_demo_code_viewer `
+           --network "host" `
+           -e ROS_MASTER_URI=http://localhost:11311 `
+           -e DISPLAY=host.docker.internal:0.0 `
+           utsma_demo_img:latest `
+           bash -c "sleep infinity"
